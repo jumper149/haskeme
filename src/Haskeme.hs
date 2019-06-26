9c5a29668b384a9adfce3def27f493209ebd40ab
Felix Springer <felixspringer149@gmail.com>
2019-06-27 01:53:23 +0200

    starting with some functions

--- src/Haskeme.hs
+++ src/Haskeme.hs
@@ -12,10 +12,41 @@
 --
 ---------------------------------------------------------------------------------------------------
 
-module Haskeme ( toLines
+module Haskeme ( IndentedLine (..)
+               , toIndentedLine
                ) where
 
--- | Splits program into lines
-toLines :: String -> [String]
-toLines prog = [prog]
+tabSize = 4
 
+data IndentedLine = IndLine Indent String
+                      deriving (Eq)
+
+instance Show IndentedLine where
+  show (IndLine n line) = [ ' ' | _ <- [1..n] ] ++ line
+
+toIndentedLine :: String -> IndentedLine
+toIndentedLine line
+  | head line == ' '  = toIndentedLineSpace (IndLine 0 line)
+  | head line == '\t' = toIndentedLineTab   (IndLine 0 line)
+  | otherwise         = IndLine 0 line
+
+toIndentedLineSpace :: IndentedLine -> IndentedLine
+toIndentedLineSpace (IndLine n line)
+  | head line == ' '  = toIndentedLineSpace (IndLine (n + 1) (tail line))
+  | head line == '\t' = error $ "Mixed indentation found in the line containing: '" ++
+                          show (toIndentedLineTab (IndLine (n + tabSize) (tail line))) ++ "'"
+  | otherwise         = (IndLine n line)
+
+toIndentedLineTab :: IndentedLine -> IndentedLine
+toIndentedLineTab (IndLine n line)
+  | head line == ' '  = error $ "Mixed indentation found in the line containing: '" ++
+                          show (toIndentedLineSpace (IndLine (n + 1) (tail line))) ++ "'"
+  | head line == '\t' = toIndentedLineTab (IndLine (n + tabSize) (tail line))
+  | otherwise         = (IndLine n line)
+
+type Indent = Int
+
+toBlocks :: Indent -> [IndentedLine] -> [IndentedLine] -> [[IndentedLine]]
+toBlocks _ [ ] block           = [ block ]
+toBlocks n ((IndLine m s):iLs) block
+  | m <= n     = block : toBlocks m iLs [ (IndLine m s) ]
