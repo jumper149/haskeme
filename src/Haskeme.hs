e9c3231f73841d2e900761f3fe904946b02fd517
Felix Springer <felixspringer149@gmail.com>
2019-06-27 00:55:42 +0200

    made ghcid available

--- src/Haskeme.hs
+++ src/Haskeme.hs
@@ -1,15 +1,14 @@
 ---------------------------------------------------------------------------------------------------
 -- |
 -- Module      : CartanSubalgebra
--- Description : Implements a Cartan Subalgebra on top of a Lie Algebra
+-- Description : Implements functions for the Haskeme executable
 -- Copyright   : (c) Felix Springer, 2019
 -- License     : BSD3
 -- Maintainer  : felixspringer149@gmail.com
 -- Stability   : experimental
 -- Portability : POSIX
 --
--- A Cartan Subalgebra can be defined on top of a Lie Algebra to later construct the Root Space,
--- which is relevant for classification of the Lie Algebra.
+-- This module implements the necessary functions for the Haskeme Main executable.
 --
 ---------------------------------------------------------------------------------------------------
 
